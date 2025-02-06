81282105e6dc8949af1501d03be429efd515d168
Fernando <palhares.f@gmail.com>
2025-02-06 15:23:26 +0100

    Project finished

--- myApp.js
+++ myApp.js
@@ -1,51 +1,27 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
+ninetyDaysInSeconds = 90 * 24 * 60 * 60
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds,
+  force: true
+}));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
 
 module.exports = app;
 const api = require('./server.js');
